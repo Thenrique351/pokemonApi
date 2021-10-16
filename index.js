f1c29ff684fb9c85711f2b19b4a0a0e9305cdd51
Tiago Rosa <75392966+Thenrique351@users.noreply.github.com>
2021-10-15 23:58:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function carregar(){
+    var data = window.document.getElementById('data')
+
+}
+
+
+/*x = dd+mm+aaaa;
+var date = document.getElementById("id").value
+if(x!= null)
+    let aux = x%251; //Número de pokemons de Jotho
+*/
+a = 251;
+for( i=2000;i<4040;i++){
+    console.log((i%a)+1)
+
+}
